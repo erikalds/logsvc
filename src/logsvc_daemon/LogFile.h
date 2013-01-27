7cd21ce1f763f829ce74919a28602ed6e6980bdc
Erik Åldstedt Sund <erikalds@gmail.com>
2013-01-27 11:54:11 +0100

    daemon LogFile class creates file if it does not exist.

---
+++ src/logsvc_daemon/LogFile.h
@@ -0,0 +1,44 @@
+#ifndef LOGFILE_H_
+#define LOGFILE_H_
+
+/* Header created: 2013-01-27
+
+  logsvc - logging as a service
+  Copyright (C) 2013 Erik Åldstedt Sund
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  To contact the author, e-mail at erikalds@gmail.com or through
+  regular mail:
+    Erik Åldstedt Sund
+    Darres veg 14
+    NO-7540 KLÆBU
+    NORWAY
+*/
+
+#include <boost/filesystem/path.hpp>
+
+namespace logsvc
+{
+  namespace daemon
+  {
+    class LogFile
+    {
+    public:
+      explicit LogFile(boost::filesystem::path& p);
+    };
+  } // namespace daemon
+} // namespace logsvc
+
+#endif // LOGFILE_H_
