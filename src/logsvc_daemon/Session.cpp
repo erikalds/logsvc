d7ad7c474b88599d61f4a4502d0f0da1917cf158
Erik Åldstedt Sund <erikalds@gmail.com>
2013-03-03 20:02:04 +0100

    Started implementing Session object

---
+++ src/logsvc_daemon/Session.cpp
@@ -0,0 +1,48 @@
+/* Source file created: 2013-03-03
+
+  logsvc - logging as a service
+  Copyright (C) 2013 Erik Åldstedt Sund
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  To contact the author, e-mail at erikalds@gmail.com or through
+  regular mail:
+    Erik Åldstedt Sund
+    Darres veg 14
+    NO-7540 KLÆBU
+    NORWAY
+*/
+
+#include "logsvc_daemon/Session.h"
+
+#include "logsvc_daemon/FileFactory.h"
+#include "log/File.h"
+
+namespace logsvc
+{
+  namespace daemon
+  {
+
+    Session::Session(FileFactory& ff) :
+      file_factory(ff)
+    {
+    }
+
+    void Session::open_file(const prot::File& f)
+    {
+      file_factory.open_file(f.get_name());
+    }
+
+  } // namespace daemon
+} // namespace logsvc
