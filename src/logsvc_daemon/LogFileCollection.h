56a369200e88b20724e1a2a6cec58dcda9edadd9
Erik Åldstedt Sund <erikalds@gmail.com>
2013-04-19 18:52:34 +0200

    Started to implement LogFileCollection

---
+++ src/logsvc_daemon/LogFileCollection.h
@@ -0,0 +1,49 @@
+#ifndef LOGFILECOLLECTION_H_
+#define LOGFILECOLLECTION_H_
+
+/* Header created: 2013-04-19
+
+  logsvc - logging as a service
+  Copyright (C) 2013 Erik Åldstedt Sund
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  To contact the author, e-mail at erikalds@gmail.com or through
+  regular mail:
+    Erik Åldstedt Sund
+    Darres veg 14
+    NO-7540 KLÆBU
+    NORWAY
+*/
+
+#include "logsvc_daemon/FileFactory.h"
+#include <boost/filesystem/path.hpp>
+
+namespace logsvc
+{
+  namespace daemon
+  {
+
+    class File;
+
+    class LogFileCollection : public FileFactory
+    {
+    public:
+      virtual std::shared_ptr<File> open_file(const boost::filesystem::path& p);
+    };
+
+  }
+} // namespace logsvc
+
+#endif // LOGFILECOLLECTION_H_
