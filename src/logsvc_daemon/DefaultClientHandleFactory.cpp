c41281bf46c78e3d7d955a0bd565b8cb99f3667b
Erik Åldstedt Sund <erikalds@gmail.com>
2013-04-28 11:44:17 +0200

    Added simple DefaultClientHandleFactory.

---
+++ src/logsvc_daemon/DefaultClientHandleFactory.cpp
@@ -0,0 +1,47 @@
+/* Source file created: 2013-04-28
+
+  logsvc - logging as a service
+  Copyright (C) 2013 Erik Åldstedt Sund
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  To contact the author, e-mail at erikalds@gmail.com or through
+  regular mail:
+    Erik Åldstedt Sund
+    Darres veg 14
+    NO-7540 KLÆBU
+    NORWAY
+*/
+
+#include "logsvc_daemon/DefaultClientHandleFactory.h"
+
+#include "log/ClientHandle.h"
+
+namespace logsvc
+{
+  namespace daemon
+  {
+
+    DefaultClientHandleFactory::DefaultClientHandleFactory() :
+      handle_counter(0)
+    {
+    }
+
+    prot::ClientHandle DefaultClientHandleFactory::generate_client_handle()
+    {
+      return prot::ClientHandle(++handle_counter);
+    }
+
+  } // namespace daemon
+} // namespace logsvc
---
+++ src/logsvc_daemon/DefaultClientHandleFactory.h
@@ -0,0 +1,51 @@
+#ifndef DEFAULTCLIENTHANDLEFACTORY_H_
+#define DEFAULTCLIENTHANDLEFACTORY_H_
+
+/* Header created: 2013-04-28
+
+  logsvc - logging as a service
+  Copyright (C) 2013 Erik Åldstedt Sund
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  To contact the author, e-mail at erikalds@gmail.com or through
+  regular mail:
+    Erik Åldstedt Sund
+    Darres veg 14
+    NO-7540 KLÆBU
+    NORWAY
+*/
+
+#include "logsvc_daemon/ClientHandleFactory.h"
+
+namespace logsvc
+{
+  namespace daemon
+  {
+
+    class DefaultClientHandleFactory : public ClientHandleFactory
+    {
+    public:
+      DefaultClientHandleFactory();
+
+      virtual prot::ClientHandle generate_client_handle();
+
+    private:
+      unsigned int handle_counter;
+    };
+
+  } // namespace daemon
+} // namespace logsvc
+
+#endif // DEFAULTCLIENTHANDLEFACTORY_H_
