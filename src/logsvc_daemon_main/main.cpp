be2ba3e2d944bcce02b1973cc1705ae4f3361a3a
Erik Åldstedt Sund <erikalds@gmail.com>
2013-05-03 15:02:47 +0200

    Implemented the main function.

--- src/logsvc_daemon_main/main.cpp
+++ src/logsvc_daemon_main/main.cpp
@@ -1,4 +1,12 @@
+#include "logsvc_daemon/SessionInitiator.h"
+#include "logsvc_daemon/DefaultSessionFactory.h"
+#include "network/DefaultSocketAcceptor.h"
+
 int main()
 {
-	return 0;
+  logsvc::daemon::DefaultSessionFactory session_factory;
+  network::DefaultSocketAcceptor socket_acceptor;
+  logsvc::daemon::SessionInitiator initiator(session_factory, socket_acceptor);
+  socket_acceptor.run();
+  return 0;
 }
