bf62c9e948355fa108e04aacbf3aa57809988836
Erik Åldstedt Sund <erikalds@gmail.com>
2013-05-11 16:08:07 +0200

    Made compile Log system test succeed.

---
+++ src/logsvccpp/Log.h
@@ -0,0 +1,45 @@
+#ifndef LOG_H_
+#define LOG_H_
+
+/* Header created: 2013-05-11
+
+  logsvc - logging as a service
+  Copyright (C) 2013 Erik Åldstedt Sund
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  To contact the author, e-mail at erikalds@gmail.com or through
+  regular mail:
+    Erik Åldstedt Sund
+    Darres veg 14
+    NO-7540 KLÆBU
+    NORWAY
+*/
+
+#include <boost/filesystem/path.hpp>
+
+namespace logsvc
+{
+  class Host;
+
+  class Log
+  {
+  public:
+    Log(const boost::filesystem::path& filename,
+        const Host& host);
+  };
+
+} // namespace logsvc
+
+#endif // LOG_H_
