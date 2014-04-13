ad0d89d2d4571452dfe7f2797cd9abf989add69c
Erik Åldstedt Sund <erikalds@gmail.com>
2014-04-13 16:00:01 +0200

    Added loglinetofile system test.

---
+++ src/systest/loglinetofile.cpp
@@ -0,0 +1,42 @@
+/* Source file created: 2014-04-13
+
+  logsvc - logging as a service
+  Copyright (C) 2013 Erik Åldstedt Sund
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  To contact the author, e-mail at erikalds@gmail.com or through
+  regular mail:
+    Erik Åldstedt Sund
+    Darres veg 14
+    NO-7540 KLÆBU
+    NORWAY
+*/
+
+#include "logsvccpp/Host.h"
+#include "logsvccpp/Log.h"
+
+int main(int argc, const char* argv[])
+{
+  if (argc < 3)
+    return 0;
+
+  logsvc::Host h("logtofile");
+  logsvc::Log log(argv[1], h);
+
+  for (int i = 1; i < argc; ++i)
+    log.logln(argv[i]);
+
+  return 0;
+}
